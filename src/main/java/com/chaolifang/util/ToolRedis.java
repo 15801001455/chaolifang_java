6181c63bb57e46eed846e451123b67483b4731c7
jinyuchuan <jinyuchuan@fang.com>
2020-12-07 09:57:17 +0800

    redis实现秒杀商品

--- src/main/java/com/chaolifang/util/ToolRedis.java
+++ src/main/java/com/chaolifang/util/ToolRedis.java
@@ -99,4 +99,41 @@ public class ToolRedis {
      *         }
      *     }
      */
+
+    //秒杀 一个商品最多500件 大家一起  这种叫秒杀
+    //参考代码
+    /**
+     *
+     *@Resource
+     *              private StringRedisTemplate stringRedisTemplate;
+     *
+     * 	    public Long flashSellByLuaScript(String skuCode,int num) {
+     * 	    	//lua脚本语法
+     * 	    	StringBuilder script= new StringBuilder();
+     * 	    	script.append(" local buyNum = ARGV[1]" );//参数
+     * 	    	script.append(" local goodsKey = KEYS[1] ");//key键
+     * 	    	script.append(" local goodsNum = redis.call('get',goodsKey) "); // 调用方式，必须是redis.call 或者pcall结合redis里的方法
+     * 	    	script.append(" if goodsNum >= buyNum  ");
+     * 	    	script.append(" then redis.call('decrby',goodsKey,buyNum)   ");
+     * 	    	script.append(" return buyNum   ");
+     * 	    	script.append(" else    ");
+     * 	    	script.append(" return '0'  ");
+     * 	    	script.append(" end");
+     *
+     * 	    	DefaultRedisScript<String> longDefaultRedisScript = new DefaultRedisScript<>(script.toString(), String.class);
+     * 	        String result = stringRedisTemplate.execute(longDefaultRedisScript, Collections.singletonList(skuCode),String.valueOf(num));
+     * 	        return Long.valueOf(result);
+     *
+     * 	    	/* List<String> keys = new ArrayList<String>();
+     * 	        RedisScript<Long> script = new DefaultRedisScript<Long>(
+     * 	            "local size = redis.call('dbsize'); return size;", Long.class);
+     * 	        Long dbsize = stringRedisTemplate.execute(script, keys, new Object[] {});
+     * 	        System.out.println("sha1:" + script.getSha1());
+     * 	        System.out.println("Lua:" + script.getScriptAsString());
+     * 	        System.out.println("dbsize:" + dbsize);
+     *
+     * 	    }
+     *
+     *
+     */
 }
